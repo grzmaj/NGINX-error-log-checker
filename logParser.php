7f76d34c17705f7971a046902afce797c7d50bfe
Grzegorz Maj <grzegorz@maj.info.pl>
2021-04-07 23:29:05 +0200

    Add colors and remove borders in table

--- logParser.php
+++ logParser.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 class logParse {
   public $logPath = "/home/alinks/logs/error.log";
 
@@ -13,17 +11,32 @@ class logParse {
 
   public function parseLogFile($logFile) {
     $dateInLogPattern = '/\d{4}\/\d{2}\/\d{2}/';
-    echo "<html><body>";
-    echo '<table>';
+    
+    echo "<html><body>\n";
+    echo '<table border=0 style="border-spacing: 0px;">';
+    $counterColor = '#ccffcc';
     foreach($logFile as $singleLog) {
       if (preg_match($dateInLogPattern, $singleLog)) {
-        echo '<tr><td style="padding: 15px;">' . $singleLog . "</td></tr>";
+
+        // Bold date and error signature
+        $separatedString = explode(" ", $singleLog, 4);
+        $logRestOfString = $separatedString[3];
+        $dateString = $separatedString[0] . " " . $separatedString[1] . " " . $separatedString[2];
+
+        if ($counter % 2 == 0) {
+          ;
+          echo '<tr><td style="border: 0px; background-color: #f5f5f0; padding: 15px;"><b>' . $dateString . '</b> ' . $logRestOfString . "</td></tr>\n";
+        }
+        else {
+          echo '<tr><td style="border: 0px; background-color: #e1e1d0; padding: 15px;"><b>' . $dateString . '</b> ' . $logRestOfString . "</td></tr>\n";
+        }
       }
       else {
-        echo '<tr><td style="padding: 5px 5px 5px 50px;">' . $singleLog . "</td></tr>";
+        echo '<tr><td style="background-color: #ccccff; padding: 5px 5px 5px 50px;">' . $singleLog . "</td></tr>\n";
       }
+      ++$counter;
     }
-    echo "</table>";
+    echo "</table>\n";
     echo "</body></html>";
   }
 }
