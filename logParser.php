3d98fb618cc45f34d0d0890c9da67b5223f13fa5
Grzegorz Maj <grzegorz@maj.info.pl>
2021-04-09 14:34:14 +0200

    Add number of log lines to display

--- logParser.php
+++ logParser.php
@@ -2,10 +2,13 @@
 
 class logParse {
   public $logPath = "/home/alinks/logs/error.log";
+  public $linesToDisplay = 30;
+  public $totalLogLines;
 
   public function openLogFile() {
     $logFile = file_get_contents($this->logPath, 'r');
     $logFile = explode("\n", $logFile);
+    $this->totalLogLines = count($logFile);
     return $logFile;
   }
 
@@ -15,29 +18,39 @@ class logParse {
     echo "<html><body>\n";
     echo '<table border=0 style="border-spacing: 0px;">';
     $counterColor = '#ccffcc';
+    $currentLogLine = 1;
+
     foreach($logFile as $singleLog) {
-      if (preg_match($dateInLogPattern, $singleLog)) {
+      // echo "<br>Line: $currentLogLine / $this->totalLogLines<br>";
 
-        // Bold date and error signature
-        $separatedString = explode(" ", $singleLog, 4);
-        $logRestOfString = $separatedString[3];
-        $dateString = $separatedString[0] . " " . $separatedString[1] . " " . $separatedString[2];
+      // Check the number of lines in log file
+      if ($this->totalLogLines - $currentLogLine <= $this->linesToDisplay && $singleLog != "") {
+        if (preg_match($dateInLogPattern, $singleLog)) {
 
-        if ($counter % 2 == 0) {
-          ;
-          echo '<tr><td style="border: 0px; background-color: #f5f5f0; padding: 15px;"><b>' . $dateString . '</b> ' . $logRestOfString . "</td></tr>\n";
+          // Bold date and error signature
+          $separatedString = explode(" ", $singleLog, 4);
+          $logRestOfString = $separatedString[3];
+          $dateString = $separatedString[0] . " " . $separatedString[1] . " " . $separatedString[2];
+  
+          if ($currentLogLine % 2 == 0) {
+            echo '<tr><td style="border: 0px; background-color: #f5f5f0; padding: 15px;"><b>' . $dateString . '</b> ' . $logRestOfString . "</td></tr>\n";
+          }
+          else {
+            echo '<tr><td style="border: 0px; background-color: #e1e1d0; padding: 15px;"><b>' . $dateString . '</b> ' . $logRestOfString . "</td></tr>\n";
+          }
         }
         else {
-          echo '<tr><td style="border: 0px; background-color: #e1e1d0; padding: 15px;"><b>' . $dateString . '</b> ' . $logRestOfString . "</td></tr>\n";
+          echo '<tr><td style="background-color: #ccccff; padding: 5px 5px 5px 50px;">' . $singleLog . "</td></tr>\n";
         }
       }
-      else {
-        echo '<tr><td style="background-color: #ccccff; padding: 5px 5px 5px 50px;">' . $singleLog . "</td></tr>\n";
-      }
-      ++$counter;
+      ++$currentLogLine;
     }
     echo "</table>\n";
     echo "</body></html>";
+  }
+
+  public function getLogSize() {
+    return filesize($this->logPath);
   }
 }
 
